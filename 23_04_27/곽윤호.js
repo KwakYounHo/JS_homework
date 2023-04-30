43b4bb30b2107c4997e9dc5a9b044efe943c6320
KwakYounHo <kwakyh951@gmail.com>
2023-04-30 13:53:17 +0900

    매개변수의 타입 체커 추가

--- 23_04_27/곽윤호.js
+++ 23_04_27/곽윤호.js
@@ -1,5 +1,9 @@
 //* 짝수인지 홀수인지 점검하는 함수
 function checkOddsEvens(number) {
+  if (typeof number !== 'number') {
+    const typeError = new Error('짝/홀수 체크 함수의 매개변수는 숫자형이어야 합니다.');
+    throw typeError;
+  }
   let result = number % 2
   switch (result) {
     case 0 :
@@ -10,11 +14,14 @@ function checkOddsEvens(number) {
 }
 
 //* 짝/홀수 점검하는 함수 테스트
-let test = [];
-for (let i=0; i<10; i++) {
-  test.push(Math.floor(Math.random() * (101)))
-}
-console.log(test.map(e=>e + ' : ' +checkOddsEvens(e)));
+// console.log(checkOddsEvens('나는 문자열이지롱'))
+// let test = [];
+// for (let i=0; i<10; i++) {
+//   test.push(Math.floor(Math.random() * (101)))
+// }
+// console.log(test.map(e=>e + ' : ' +checkOddsEvens(e)));
+// console.log(checkOddsEvens(-4152322));
+// console.log(checkOddsEvens(-4152321));
 
 
 //* 엘레베이터
